d7fe05df8d9df89f68816919852494ff8f468d76
Ocloa <lexasen231@mail.ru>
2024-04-08 16:20:30 +0700

    disabled undefined check for session state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import Message from './Message';
 
 
 export default async function Home() {
-  const session = await getServerSession();
-  
+  const session:any = await getServerSession();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Message isLogged={!!session} name={session?.user.name}></Message>
